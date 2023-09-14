fe26ba0d820af44f7c7a3e630bf7ebd1556abba6
Marcos Levy <marcoslevy_silveira@hotmail.com>
2023-09-14 16:22:07 -0400

    Consult with many criteria

--- src/main/java/com/studies/workshopmongo/services/PostService.java
+++ src/main/java/com/studies/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.studies.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,7 +23,12 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
-	public List<Post> findByTitle(String text){
+	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
